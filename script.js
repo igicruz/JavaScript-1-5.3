86df0fbf2564b00ec851be15e73d700cdd312db1
igicruz <ialderete12@gmail.com>
2017-03-18 00:01:44 -0700

    add file
    add file

---
+++ script.js
@@ -0,0 +1,23 @@
+/*JS*/
+
+var process = {
+	employee :"Name: Ignacio",
+	store : 'Store: Milan',
+	edge : "Age: 41",
+	salary : "Salary $200",
+	address : "Address: 825 Dexter St, Milan, MI 48160",
+	
+				
+	
+	displayName : function(){
+	
+	console.log(this.employee + " " + this.store + " " +
+				this.edge + " " + this.salary + " " + this.address)
+	
+	}
+}
+process.displayName();
+
+
+	
+		
